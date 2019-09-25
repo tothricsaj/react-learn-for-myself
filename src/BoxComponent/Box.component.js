72cf9bbeb97ee7806a4d2cca9f808508dacf5541
tothricsaj <tothricsaj@gmail.com>
2019-09-25 15:51:37 +0200

    eventHandling/smile face in a div

--- src/BoxComponent/Box.component.js
+++ src/BoxComponent/Box.component.js
@@ -4,11 +4,23 @@ import styles from './Box.module.css'; // important --> only dot and module and 
 class BoxComponent extends React.Component {
     constructor(props) {
         super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            isToggleOn: true,
+        }
+    }
+
+    handleClick() {
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+        }));
     }
 
     render() {
         return <div className={ styles.wrapperBox }>
             <h2 className={ styles.h2Class }>{ this.props.msg }</h2>
+            <div className={styles.buttonDiv} onClick={this.handleClick}>{this.state.isToggleOn? ':)' : ':('}</div>
         </div>
     }
 }
