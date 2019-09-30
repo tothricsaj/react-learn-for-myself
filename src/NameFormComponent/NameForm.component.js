6ef13e86566d6609707c06efcb19635928e51508
tothricsaj <tothricsaj@gmail.com>
2019-09-30 07:16:55 +0200

    nameformcomponent is added

---
+++ src/NameFormComponent/NameForm.component.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class NameForm extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <form action="">
+
+            </form>
+        );
+    }
+}
+
+export default NameForm;
