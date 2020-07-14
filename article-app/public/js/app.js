1467a66ba321fbc2f3cdcc02a776ca06d5569998
tothricsaj <tothricsaj@gmail.com>
2020-07-14 15:41:50 +0200

    article layout

--- article-app/public/js/app.js
+++ article-app/public/js/app.js
@@ -1,14 +1,30 @@
-class ArctilceList extends React.Component {
+class ArticleList extends React.Component {
     render() {
         return(
             <div className="articleWraper">
-                Test Text
+                <Article />
             </div>
         )
     }
 }
 
+class Article extends React.Component {
+    render() {
+        return(
+            <div>
+                <h2>Article title</h2>
+                <div>
+                    Lead TextLead TextLead TextLead TextLead TextLead TextLead TextLead TextLead TextLead TextLead TextLead TextLead TextLead TextLead TextLead TextLead TextLead TextLead TextLead TextLead TextLead Text
+                </div>
+                <p>Author: Kitty Man</p>
+                <p>2020/07/14</p>
+            </div>
+        )
+    }
+}
+
+
 ReactDOM.render(
-    <ArctilceList />,
+    <ArticleList />,
     document.querySelector('#app')
 )
