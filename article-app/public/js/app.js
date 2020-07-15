7f5e2020adeb5d487c909a08e181deb9370d02fb
tothricsaj <tothricsaj@gmail.com>
2020-07-15 07:29:43 +0200

    added images / article layout with image :fire:

--- article-app/public/js/app.js
+++ article-app/public/js/app.js
@@ -12,10 +12,15 @@ class Article extends React.Component {
     render() {
         return(
             <div className="article">
-                <div className="articleTitleWrapper">
-                    <h2 className="articleTitle">
-                        Article title Article title Article titleArticle titleArticle titleArticle titleArticle titleArticle titleArticle titleArticle titleArticle titleArticle title
-                    </h2>
+                <div className="articleHead">
+                    <div className="articleImage">
+                        <img src="../images/evil_cat.jpg" />
+                    </div>
+                    <div className="articleTitleWrapper">
+                        <h2 className="articleTitle">
+                            Article title Article title Article titleArticle titleArticle titleArticle titleArticle titleArticle titleArticle titleArticle titleArticle titleArticle title
+                        </h2>
+                    </div>
                 </div>
                 <div className="delimiter"></div>
                 <div className="articleLead">
