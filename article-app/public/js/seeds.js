c6918b469a536f0e32fd2c4cd8fb411b87dc3e8a
tothricsaj <tothricsaj@gmail.com>
2020-07-15 07:39:35 +0200

    seeds.js is added

---
+++ article-app/public/js/seeds.js
@@ -0,0 +1,12 @@
+window.Seeds = (function() {
+    function generateClickCount() {
+        return null
+    }
+
+    const articles = [
+        'foobarbaz'
+    ]
+
+    return {articles: articles}
+
+}())
